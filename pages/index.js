2633407a0d251483462a9dcf956f83404b1e0aa5
mm0hammadi <mm34549@gmail.com>
2021-08-08 14:09:16 +0430

    filter country feature

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,31 @@ import CountriesTable from "../componens/CountriesTable/CountriesTable";
 import Layout from "../componens/Layout/Layout";
 import SearchInput from "../componens/SearchInput/SearchInput";
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
 
 export default function Home({ countries }) {
-  console.log(countries);
+  // console.log(countries);
+  const [keyword, setKeyword] = useState("");
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  );
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+    setKeyword(e.target.value.toLowerCase());
+  };
+
   return (
     <Layout>
       <div className="styles.counts">Found{countries.length} countries</div>
-      <SearchInput placeholder="Filter by Name, Region subRegion " />
-      <CountriesTable countries={countries} />
+      <SearchInput
+        onChange={onInputChange}
+        placeholder="Filter by Name, Region subRegion "
+      />
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   );
 }
